0e8cb531d158d22b75fd75e5a86206e2179b1eb8
assilbekov <askhat.assilbekov@gmail.com>
2024-05-01 17:57:47 -0300

    add VendingMachine

---
+++ pkg/vending-machine.go
@@ -0,0 +1,11 @@
+package pkg
+
+type VendingMachine struct {
+	hasItem       State
+	itemRequested State
+	hasMoney      State
+	noItem        State
+	currentState  State
+	itemCount     int
+	itemPrice     int
+}
