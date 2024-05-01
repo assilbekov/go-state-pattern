aaa5cb09a45a54d1c2beb8cea8d3a7886838e5d8
assilbekov <askhat.assilbekov@gmail.com>
2024-05-01 18:00:16 -0300

    make VendingMachine implement state

--- pkg/vending-machine.go
+++ pkg/vending-machine.go
@@ -9,3 +9,23 @@ type VendingMachine struct {
 	itemCount     int
 	itemPrice     int
 }
+
+func (v *VendingMachine) RequestItem() error {
+	return v.currentState.RequestItem()
+}
+
+func (v *VendingMachine) AddItem(count int) error {
+	return v.currentState.AddItem(count)
+}
+
+func (v *VendingMachine) InsertMoney(money int) error {
+	return v.currentState.InsertMoney(money)
+}
+
+func (v *VendingMachine) DispenseItem() error {
+	return v.currentState.DispenseItem()
+}
+
+func (v *VendingMachine) SetState(s State) {
+	v.currentState = s
+}
